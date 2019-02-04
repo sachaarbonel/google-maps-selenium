a9b4dca0fa5ccf641b362e3b3157ae0e6c815bad
Sach97 <sacha.arbonel@hotmail.fr>
2019-02-04 18:47:07 +0100

    refactor find and click

--- maps.py
+++ maps.py
@@ -9,11 +9,17 @@ from bs4 import BeautifulSoup
 import queue
 
 
-def wait_and_find_element_by_xpath(waitDriver: WebDriverWait, xpath: str)->str:
+def wait_and_find(waitDriver: WebDriverWait, xpath: str)->str:
 
     element = waitDriver.until(EC.visibility_of_element_located(
         (By.XPATH, xpath))).find_element_by_xpath(xpath)
     return element.text
+
+
+def wait_and_click(waitDriver: WebDriverWait, xpath: str):
+    page = waitDriver.until(EC.element_to_be_clickable(
+        (By.XPATH, xpath)))  # result detail page
+    page.click()
 
 
 if __name__ == "__main__":
@@ -21,26 +27,25 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
 
     driver.get(
-        'https://www.google.com/maps/search/H%C3%B4tels/@14.4964286,-61.0759903,13z')
+        'https://www.google.com/maps/search/H%C3%B4tels/@14.4964286,-61.0759903,13z')  # lat, long, zoom level
 
     wait = WebDriverWait(driver, 10)
 
     results = []
-    for x in driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "section-result", " " ))]'):
-        results.append(x.get_attribute('data-result-index'))
+    for elt in driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "section-result", " " ))]'):
+        results.append(elt.get_attribute('data-result-index'))
 
     for r in results:
         print(r)
 
     r = "1"
-    page = wait.until(EC.element_to_be_clickable(
-        (By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "section-result", " " )) and (((count(preceding-sibling::*) + 1) = '+r+') and parent::*)]')))  # result detail page
-    page.click()
+    section_result_xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "section-result", " " )) and (((count(preceding-sibling::*) + 1) = '+r+') and parent::*)]'
+    wait_and_click(wait, section_result_xpath)
     phone_number_xpath = '//*[@id="pane"]/div/div[1]/div/div/div[18]/div/div[1]/span[3]/span[3]'
-    phone = wait_and_find_element_by_xpath(wait, phone_number_xpath)
+    phone = wait_and_find(wait, phone_number_xpath)
     print(phone)
     hotel_name_xpath = '//*[@id="pane"]/div/div[1]/div/div/div[1]/div[3]/div[1]/h1'
-    hotel = wait_and_find_element_by_xpath(wait, hotel_name_xpath)
+    hotel = wait_and_find(wait, hotel_name_xpath)
     print(hotel)
     driver.back()
     driver.forward()
