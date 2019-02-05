9b767570100c8637e0e0b5ef6ba9dc90b24fd88b
Sach97 <sacha.arbonel@hotmail.fr>
2019-02-05 11:30:48 +0100

    sleep 2 seconds

--- maps.py
+++ maps.py
@@ -22,12 +22,20 @@ def wait_and_click(waitDriver: WebDriverWait, xpath: str):
     page.click()
 
 
+def hotel_xpath(r):
+    index = 1
+    newindex = r*2-index
+    div_id = str(newindex)
+    section_result_xpath = '//*[@id="pane"]/div/div[1]/div/div/div[4]/div['+div_id+']'
+    return section_result_xpath
+
+
 if __name__ == "__main__":
 
     driver = webdriver.Chrome()
-
+    url = 'https://www.google.com/maps/search/H%C3%B4tels/@14.4964286,-61.0759903,13z'
     driver.get(
-        'https://www.google.com/maps/search/H%C3%B4tels/@14.4964286,-61.0759903,13z')  # lat, long, zoom level
+        url)  # lat, long, zoom level
 
     wait = WebDriverWait(driver, 10)
 
@@ -38,8 +46,8 @@ if __name__ == "__main__":
     for r in results:
         print(r)
 
-    r = "1"
-    section_result_xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "section-result", " " )) and (((count(preceding-sibling::*) + 1) = '+r+') and parent::*)]'
+    r = 1
+    section_result_xpath = hotel_xpath(r)
     wait_and_click(wait, section_result_xpath)
     phone_number_xpath = '//*[@id="pane"]/div/div[1]/div/div/div[18]/div/div[1]/span[3]/span[3]'
     phone = wait_and_find(wait, phone_number_xpath)
@@ -47,5 +55,20 @@ if __name__ == "__main__":
     hotel_name_xpath = '//*[@id="pane"]/div/div[1]/div/div/div[1]/div[3]/div[1]/h1'
     hotel = wait_and_find(wait, hotel_name_xpath)
     print(hotel)
+    time.sleep(2)
     driver.back()
-    driver.forward()
+    # driver.forward()
+    current_url = driver.current_url
+    print(current_url)
+
+    # driver.refresh()
+    # print("driver refreshed")
+    r = 2
+    s = hotel_xpath(r)
+    wait_and_click(wait, s)
+    print("clicked")
+    time.sleep(2)
+    phone2 = wait_and_find(wait, phone_number_xpath)
+    print(phone2)
+    print(driver.current_url)
+    driver.quit()
